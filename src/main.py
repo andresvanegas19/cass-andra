ed5a7dacbd4d6270f6ce2bb25a726e24d4430d1e
“andresvanegas19” <andressantiagore@gmail.com>
2022-05-01 16:22:06 -0500

    test with the previus model light

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from src.data.load.load_datasets import nasqad_historical, download_csv_load_dri
 from src.processing.modify_columns import change_date_to_column, change_column_name, nasqad
 from src.processing.merge import concat_dataset_to_one
 from src.processing.reshaping import normalize_model
+from src.processing.cleaning import test
 # from src.data.load.scraping import coinmarket
 # from src.data.load.load_datasets import download_csv_load_drive
 # print(download_csv_load_drive())
@@ -16,18 +17,19 @@ from src.processing.reshaping import normalize_model
 
 
 nasqad_data = nasqad_historical()
-bistamp = download_csv_load_drive()
+test(nasqad_data)
+# bistamp = download_csv_load_drive()
 
-change_date_to_column(bistamp)
-df_bistamp = change_column_name(
-    bistamp, ["Open", "High",	"Low",	"Close", "Volume_(BTC)"])
-# print(df_bistamp)
-df_nasqad = nasqad(nasqad_data)
-# print(df_nasqad)
+# change_date_to_column(bistamp)
+# df_bistamp = change_column_name(
+#     bistamp, ["Open", "High",	"Low",	"Close", "Volume_(BTC)"])
+# # print(df_bistamp)
+# df_nasqad = nasqad(nasqad_data)
+# # print(df_nasqad)
 
-df = concat_dataset_to_one(df_bistamp, df_nasqad)
+# df = concat_dataset_to_one(df_bistamp, df_nasqad)
 
-# normalize_model
-# print(df)
-model = normalize_model(df)
-print(model.describe())
+# # normalize_model
+# # print(df)
+# model = normalize_model(df)
+# print(model.describe())
--- src/processing/cleaning.py
+++ src/processing/cleaning.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """ Module for cleaning data """
 
+from tensorflow.keras import layers, Sequential
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 
 from processing.utils import change_date_formate
@@ -42,3 +45,60 @@ def cleaning():
     n_trade.set_index("Time", inplace=True)
     n_trade["n_trade"] = n_trade.sum(axis=1)
     n_trade = n_trade[["n_trade"]]
+
+
+SEQ_LEN = 100
+
+
+def to_sequences(data, seq_len):
+    d = []
+    for index in range(len(data) - seq_len):
+        d.append(data[index: index + seq_len])
+
+    return np.array(d)
+
+
+def preprocess(data_raw, seq_len, train_split):
+    data = to_sequences(data_raw, seq_len)
+    num_train = int(train_split * data.shape[0])
+
+    X_train = data[:num_train, :-1, :]
+    y_train = data[:num_train, -1, :]
+    X_test = data[num_train:, :-1, :]
+    y_test = data[num_train:, -1, :]
+
+    return X_train, y_train, X_test, y_test
+
+
+def test(dataframe):
+    scaler = MinMaxScaler()
+
+    scaled_close = scaler.fit_transform(
+        dataframe.Close.values.reshape(-1, 1)
+    )
+    scaled_close = scaled_close[~np.isnan(scaled_close)].reshape(-1, 1)
+    X_train, y_train, X_test, y_test =\
+        preprocess(scaled_close, SEQ_LEN, train_split=0.80)
+    model = Sequential()
+    model.add(layers.LSTM(units=32, return_sequences=True,
+                          input_shape=(99, 1), dropout=0.2))
+    model.add(layers.LSTM(units=32, return_sequences=True,
+                          dropout=0.2))
+    model.add(layers.LSTM(units=32, dropout=0.2))
+    model.add(layers.Dense(units=1))
+    # model.summary()
+    model.compile(
+        loss='mean_squared_error',
+        optimizer='adam'
+    )
+    BATCH_SIZE = 64
+    model.fit(
+        X_train,
+        y_train,
+        epochs=30,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        validation_split=0.1
+    )
+
+    model.save('bitcoin.h5')
